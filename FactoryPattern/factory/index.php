3b23403b3ccbbe44ccb6e0f4ca9452aa630b1d22
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-22 18:39:51 +0200

    .

--- FactoryPattern/factory/index.php
+++ FactoryPattern/factory/index.php
@@ -2,7 +2,7 @@
 require_once './lib/booking.php';
 require_once './lib/BookingInstance.php';
 
-$type = 'barca';
+$type = 'camera';
 
 $booking = BookingInstance::getInstance($type);
 $booking->price();
